abf9db3e361cd6da6c669d3f8ae57c77c71f536e
Your Name <thilakamuthukrishnan@gmail.com>
2026-09-26 12:25:05 +0530

    upload sql files

---
+++ truncate table company_dim restart ident.sql
@@ -0,0 +1,4 @@
+truncate table company_dim restart identity cascade;
+truncate table skills_dim restart identity cascade;
+truncate table job_postings_fact restart identity cascade;
+truncate table skills_job_dim restart identity cascade;
